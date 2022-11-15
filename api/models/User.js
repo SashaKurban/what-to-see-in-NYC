d5779cdc8fb636b89f4102f7c91d6b4a8a71e725
SashaKurban <sasha_kurbanova@yahoo.com>
2022-11-15 12:09:21 -0500

    authentication with error when signup

--- api/models/User.js
+++ api/models/User.js
@@ -1,41 +1,32 @@
-"use strict";
+// "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {}
 
   User.init(
     {
-        username: {
-            type: DataTypes.STRING,
-            validate: {
-              len: [3, 250],
-              notEmpty: true,
-            },
-            unique: true,
-          },
-        bio: {
-            type: DataTypes.STRING,
-            validate: {
-              len: [3, 250],
-              notEmpty: true,
-            }
-        },
+        username: {type: DataTypes.STRING},
+        bio: {type: DataTypes.STRING},
         email: {
             type: DataTypes.STRING,
             validate: {
-              len: [3, 250],
-              notEmpty: true,
               isEmail: true,
             },
             unique: true,
+            allowNull: false,
         },
+        passwordHash: { type: DataTypes.STRING },
         password: {
-            type: DataTypes.STRING,
-            validate: {
-              len: [3, 250],
-              notEmpty: true,
+          type: DataTypes.VIRTUAL,
+          validate: {
+            isLongEnough: (val) => {
+              if (val.length < 7) {
+                throw new Error("Please choose a longer password");
+              }
             }
+          }
         },
     },
     {
@@ -46,9 +37,15 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = (models) => {
     // associations can be defined here
-    models.User.belongsTo(models.Category);
-    models.User.belongsToMany(models.Event, { through: "UserEvents" });
+    // models.User.belongsTo(models.Category);
+    // models.User.belongsToMany(models.Event, { through: "UserEvents" });
   };
+
+  User.beforeSave((user, options) => {
+    if (user.password) {
+      user.passwordHash = bcrypt.hashSync(user.password, 10);
+    }
+  });
 
   return User;
 };
