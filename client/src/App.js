314a561e501df12dbea680d5db6f9a0d12594d09
Sheyla V Perez <42497841+sheylavperez@users.noreply.github.com>
2022-11-21 14:01:27 -0500

    added a login/signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,83 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import PostsListPage from "./pages/PostsListPage";
 import PostFormPage from "./pages/PostFormPage";
 import ShowPostPage from "./pages/ShowPostPage";
-import AboutUsPage from "./pages/AboutUsPage";
+import LoginPage from "./pages/LoginPage";
+
+import HomePage from "./pages/HomePage";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import MuseumsPage from "./pages/MuseumsPage";
+// import ParksPage from "./pages/ParksPage";
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+// import Nav from "./pages/Nav";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
 
 import "./App.css";
 
+
+
 function Navigation(props) {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Micro Blog
-        </Link>
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/posts/new">
-              Create a Micro Post
+        
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className=" ms-auto">
+   
+            {/* <Nav.Link href="#link"> Categories</Nav.Link> */}
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/museums">MUSEUMS and ART INSTITUTIONS</NavDropdown.Item>
+              <NavDropdown.Item href="/parks">PARKS and PUBLIC SPACE</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">SHOWS and CONCERTS</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavLink className="nav-link" to="/log-in">
+            Login
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/about-us">
-              About Us
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+
+
+
+            
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 }
 
 function App() {
+ 
   return (
     <BrowserRouter>
       <Navigation />
-      <div className="container-xl text-center">
-        <div className="row justify-content-center">
+     
+ 
+         
           <Routes>
-            <Route path="/posts/new" element={<PostFormPage />} />
-            <Route path="/posts/:id" element={<ShowPostPage />} />
-            <Route path="/about-us" element={<AboutUsPage />} />
-            <Route path="/" element={<PostsListPage />} />
+    
+            <Route path="/shows" element={<showsPage />} />
+            {/* <Route path="/parks" element={<ParksPage />} /> */}
+            <Route path="/museums" element={<MuseumsPage />} />
+            <Route path="/log-in" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
           </Routes>
-        </div>
-      </div>
+     
     </BrowserRouter>
+
+    
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
 
 // React 17 style loading of application
 ReactDOM.render(<App />, document.getElementById("root"));
