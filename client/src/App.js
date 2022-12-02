99c3533703ecdd65c3ce763c8d9f208804569ed4
ravenclaw1999 <702schowdhury@gmail.com>
2022-12-01 19:38:38 -0500

    initial frontend commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import PostsListPage from "./pages/PostsListPage";
 import PostFormPage from "./pages/PostFormPage";
 import ShowPostPage from "./pages/ShowPostPage";
 import AboutUsPage from "./pages/AboutUsPage";
+import HomePage from "./pages/HomePage";
+import UserProfile from "./pages/UserProfile";
+import CreateEvent from "./pages/CreateEvent";
 
 import "./App.css";
 
@@ -12,7 +15,7 @@ function Navigation(props) {
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Micro Blog
+          HomePage
         </Link>
         <ul className="navbar-nav me-auto">
           <li className="nav-item">
@@ -23,6 +26,11 @@ function Navigation(props) {
           <li className="nav-item">
             <NavLink className="nav-link" to="/about-us">
               About Us
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/user-profile">
+              User Profile
             </NavLink>
           </li>
         </ul>
@@ -38,10 +46,12 @@ function App() {
       <div className="container-xl text-center">
         <div className="row justify-content-center">
           <Routes>
+          <Route path="/" element={<HomePage />} />
             <Route path="/posts/new" element={<PostFormPage />} />
             <Route path="/posts/:id" element={<ShowPostPage />} />
             <Route path="/about-us" element={<AboutUsPage />} />
-            <Route path="/" element={<PostsListPage />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            <Route path="/create-event" element={<CreateEvent />} />
           </Routes>
         </div>
       </div>
---
+++ client/src/pages/PastEvents.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/esm/Container";
+import Form from 'react-bootstrap/Form';
+import Image from 'react-bootstrap/Image'
+
+export default function CreateEvent() {
+    return (
+        <div>
+            <h1>What to See in NYC</h1>
+            <h2>View Past Events</h2>
+
+            <Container>
+                <Row>
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
