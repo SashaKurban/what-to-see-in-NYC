518c3541a65f766ead0b8d7de4ff24a9fe38cbfe
SashaKurban <sasha_kurbanova@yahoo.com>
2022-12-14 10:51:14 -0500

    removed micropots model

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -15,6 +15,7 @@ router.post("/signup", (req, res) => {
     });
 });
 
+
 router.post("/login", passport.authenticate("local"), (req, res) => {
   // If this function gets called, authentication was successful.
   // `req.user` contains the authenticated user.
